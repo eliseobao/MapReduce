f43e513c4b6703361c8294d059e442f92981c699
eliseobao <eliseo.bao@udc.es>
2022-11-10 19:52:29 +0100

    Exercise 2 now prints all urls with most visits

--- src/exercise_2/combiner.py
+++ src/exercise_2/combiner.py
@@ -5,12 +5,15 @@ with open("exercise_2_results.txt") as file:
 
     for line in file:
         item, count = line.strip().split('\t')
+
         if item.startswith("url-"):
             urls.append((item[4:], int(count)))
         else:
             users.append((item[4:], int(count)))
 
-    urls.sort(key=itemgetter(1)), users.sort(key=itemgetter(1))
+    most_visits, most_ps = max(urls, key=itemgetter(1))[1], max(users, key=itemgetter(1))[1]
+    most_visited_urls = [url[0] for url in urls if most_visits in url]
+    users_most_ps = [user[0] for user in users if most_ps in user]
 
-    print("Most visited URL is " + urls[-1][0] + " (" + str(urls[-1][1]) + " visits)")
-    print("User with most '.ps' files visited is " + users[-1][0] + ' (' + str(users[-1][1]) + " visits)")
+    print("Most visited URL(s) is/are " + str(most_visited_urls) + " (" + str(most_visits) + " visits)")
+    print("User(s) with most '.ps' files visited is/are " + str(users_most_ps) + ' (' + str(most_ps) + " visits)")
