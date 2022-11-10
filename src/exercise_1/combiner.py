2d2dc87b25a617985822a61371621dabc9071169
eliseobao <eliseo.bao@udc.es>
2022-11-10 19:36:33 +0100

    Exercise 1 now prints all cities with hottest/coldest temperature

--- src/exercise_1/combiner.py
+++ src/exercise_1/combiner.py
@@ -1,13 +1,18 @@
 from operator import itemgetter
 
 with open("exercise_1_results.txt") as file:
-    lines = []
+    results = []
+
     for line in file:
         town, temp = line.strip().split('\t', 1)
-        lines.append((town, float(temp)))
-    lines = [tup for tup in lines if tup[1] != 0]
-    lines.sort(key=itemgetter(1))
+        results.append((town, float(temp)))
 
-    print('Hottest temperature: ' + str(lines[-1][1]) + ' in ' + lines[-1][0])
-    print('Coldest temperature: ' + str(lines[0][1]) + ' in ' + lines[0][0])
+    lines = [tup for tup in results if tup[1] != 0]
+
+    hottest_temp, coldest_temp = max(results, key=itemgetter(1))[1], min(results, key=itemgetter(1))[1]
+    hottest_cities = [city[0] for city in results if hottest_temp in city]
+    coldest_cities = [city[0] for city in results if coldest_temp in city]
+
+    print('Hottest temperature: ' + str(hottest_temp) + ' in ' + str(hottest_cities))
+    print('Coldest temperature: ' + str(coldest_temp) + ' in ' + str(coldest_cities))
 
